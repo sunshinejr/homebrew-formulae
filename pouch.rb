f7fc52e19ad31d645e360f06cfb4fc0f9469c190
Łukasz Mróz <thesunshinejr@gmail.com>
2021-03-24 17:25:13 +0100

    Update pouch formula a bit

--- pouch.rb
+++ pouch.rb
@@ -1,13 +1,12 @@
 class Pouch < Formula
-  desc "Secret management for Swift projects."
+  desc "Secret management for Swift projects"
   homepage "https://github.com/sunshinejr/Pouch"
-  url "https://github.com/sunshinejr/Pouch/releases/download/0.1.0/pouch.zip"
-  sha256 "436536099df66d620f5d515b1b568b7c7b64b20939f55d9c79a324719274012d"
-  license "MIT"
-  head "https://github.com/sunshinejr/Pouch.git"
   version "0.1.0"
+  url "https://github.com/sunshinejr/Pouch/archive/#{version}.tar.gz"
+  sha256 "423c7c40dc051a1145990d309b4c6322c2fdfce0952b284d1c0a8fed97df21c9"
+  head "https://github.com/sunshinejr/Pouch.git"
 
-  depends_on xcode: ["12.0", :build]
+  depends_on :xcode => ["12", :build]
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
