ea73f69310722c15fb9313293bc5675813f62b00
Łukasz Mróz <thesunshinejr@gmail.com>
2021-04-02 09:17:42 +0200

    Update Pouch formula to 0.2.0

--- pouch.rb
+++ pouch.rb
@@ -1,9 +1,9 @@
 class Pouch < Formula
   desc "Secret management for Swift projects"
   homepage "https://github.com/sunshinejr/Pouch"
-  version "0.1.2"
+  version "0.2.0"
   url "https://github.com/sunshinejr/Pouch/archive/#{version}.tar.gz"
-  sha256 "6ce02fe34c78994f79a297b0854908505353748fab57308a22220e61fd900683"
+  sha256 "f0dad64da6587f120b10261ae19bf99bddd2953022134c08d4308f79f15d385c"
   head "https://github.com/sunshinejr/Pouch.git"
 
   depends_on :xcode => ["12", :build]
